e6d00f52afe7f77075b4bb9b8400e0c5a79cde2d
Eraly <eraly.madishov@gmail.com>
2024-04-11 10:18:54 +0600

    Half of Profile page finished and added AppColors and AppTheme

---
+++ lib/features/profile/presentation/widgets/list_style_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ListStileWidget extends StatelessWidget {
+  const ListStileWidget({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    this.trailing,
+    required this.theme,
+    this.leading,
+    this.onTap,
+  });
+  final Widget? leading;
+  final String title;
+  final String subtitle;
+  final Widget? trailing;
+  final Function()? onTap;
+
+  final ThemeData theme;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onTap,
+      contentPadding: EdgeInsets.zero,
+      leading: leading,
+      title: Text(
+        title,
+        style:
+            theme.textTheme.bodyMedium!.copyWith(fontWeight: FontWeight.w700),
+      ),
+      subtitle: Text(
+        subtitle,
+        style: theme.textTheme.bodySmall!.copyWith(color: theme.disabledColor),
+      ),
+      trailing: trailing,
+    );
+  }
+}
