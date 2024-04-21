60559e2442bb1791cbe1f761fbf63228eecf9e87
UsOUse4k <usman.omuraliev.use4k@gmail.com>
2024-04-21 12:52:43 +0600

    fix(entry-point): Wrapped MaterialApp with BlocProviders

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,14 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:w_sharme_beauty/core/di/injector.dart';
-import 'package:w_sharme_beauty/firebase_options.dart';
 import 'package:w_sharme_beauty/features/app/widgets/app.dart';
+import 'package:w_sharme_beauty/firebase_options.dart';
 
 Future<void> lazyBootstrap(
   WidgetsBinding widgetsBinding,
   String environment,
 ) async {
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   configureDependencies(environment: environment);
 
