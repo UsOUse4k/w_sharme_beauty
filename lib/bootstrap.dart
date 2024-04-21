06b151cfa93cb8e8f9efd846b635bd1d1f2b8be3
UsOUse4k <usman.omuraliev.use4k@gmail.com>
2024-04-21 12:37:26 +0600

    refactor(folders-and-files): Deleted unnecessary .gitkeep files. Moved firebase_options.dart file to lib folder

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:w_sharme_beauty/core/di/injector.dart';
-import 'package:w_sharme_beauty/core/utils/firebase_options.dart';
+import 'package:w_sharme_beauty/firebase_options.dart';
 import 'package:w_sharme_beauty/features/app/widgets/app.dart';
 
 Future<void> lazyBootstrap(
