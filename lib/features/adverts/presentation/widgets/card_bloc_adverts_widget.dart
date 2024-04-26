c5ed10133c19ee0c176b4aa6f7caba07f8ec542f
Oroz Jumabekov <oroz7307@gmail.com>
2024-04-26 18:29:31 +0600

    fixed the route

--- lib/features/adverts/presentation/widgets/card_bloc_adverts_widget.dart
+++ lib/features/adverts/presentation/widgets/card_bloc_adverts_widget.dart
@@ -20,68 +20,67 @@ class CardBlocAdvertsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        color: AppColors.white,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18),
-            child: CardBlocNavbar(
-              title: title,
-              desc: desc,
-              rating: rating,
+    return GestureDetector(
+      onTap: () {
+        context.push('/adverts/${RouterContants.advertDetailPage}');
+      },
+      child: Container(
+        decoration: const BoxDecoration(
+          color: AppColors.white,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18),
+              child: CardBlocNavbar(
+                title: title,
+                desc: desc,
+                rating: rating,
+              ),
             ),
-          ),
-          const SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.only(left: 18),
-            child: SizedBox(
-              height: 85,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      context
-                          .push('/adverts/${RouterContants.advertDetailPage}');
-                    },
-                    child: ClipRRect(
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(left: 18),
+              child: SizedBox(
+                height: 85,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return ClipRRect(
                       borderRadius: BorderRadius.circular(10),
                       child: Assets.images.bloc.image(
                         width: 80,
                         height: 80,
                         fit: BoxFit.cover,
                       ),
-                    ),
-                  );
-                },
-                separatorBuilder: (BuildContext context, int index) =>
-                    const SizedBox(width: 6),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const SizedBox(width: 6),
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Работаю до 22:00',
-                  style: AppStyles.w400f14,
-                ),
-                Text(
-                  '2 км.',
-                  style: AppStyles.w400f14.copyWith(color: AppColors.grey),
-                ),
-              ],
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Работаю до 22:00',
+                    style: AppStyles.w400f14,
+                  ),
+                  Text(
+                    '2 км.',
+                    style: AppStyles.w400f14.copyWith(color: AppColors.grey),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
