a9b6e462292cb769d3eed0e52746f0184753edbe
Oroz Jumabekov <oroz7307@gmail.com>
2024-06-04 01:55:09 +0600

    fixed

--- lib/features/chat_group/presentation/widgets/card_chat_group_widget.dart
+++ lib/features/chat_group/presentation/widgets/card_chat_group_widget.dart
@@ -3,8 +3,11 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:w_sharme_beauty/core/router/router.dart';
 import 'package:w_sharme_beauty/core/theme/app_colors.dart';
+import 'package:w_sharme_beauty/core/theme/app_styles.dart';
+import 'package:w_sharme_beauty/core/utils/format_date/format_chat_time.dart';
 import 'package:w_sharme_beauty/core/widgets/widgets.dart';
 import 'package:w_sharme_beauty/features/communities/domain/entities/community/entities.dart';
+import 'package:w_sharme_beauty/features/post/presentation/widgets/post_card_widget.dart';
 
 class CardChatGroupWidget extends StatelessWidget {
   const CardChatGroupWidget({
@@ -21,6 +24,9 @@ class CardChatGroupWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final router = GoRouter.of(context);
+    final bool isCurrenUser =
+        firebaseAuth.currentUser!.uid == community.lastSenderId.toString();
+    final formatTime = formatChatTime(community.lastMessageTs!);
     return Column(
       children: [
         Container(
@@ -42,20 +48,53 @@ class CardChatGroupWidget extends StatelessWidget {
                   raduis: 30,
                   avatar: community.chatImageUrl,
                   name: community.chatGroupName.toString(),
-                  subTitle: "пусто",
+                  subTitle: community.lastMessage ?? '',
                 ),
-                const Column(
+                Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.end,
                   // ignore: avoid_redundant_argument_values
                   children: [
-                    // для отслеживания
+                    Row(
+                      children: [
+                        if (isCurrenUser)
+                          Icon(
+                            community.seen != null && community.seen!
+                                ? Icons.done_all
+                                : Icons.check,
+                            size: 16,
+                            color: community.seen!
+                                ? AppColors.purple
+                                : AppColors.grey,
+                          ),
+                        Text(
+                          formatTime,
+                          style:
+                              AppStyles.w500f14.copyWith(color: AppColors.grey),
+                        ),
+                      ],
+                    ),
+                    if (!isCurrenUser &&
+                        community.messageCount != null &&
+                        community.messageCount! > 0)
+                      Container(
+                        width: 20.w,
+                        height: 20.h,
+                        decoration: BoxDecoration(
+                          color: AppColors.purple,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            '${community.messageCount}',
+                            style: AppStyles.w400f14.copyWith(
+                              color: AppColors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    Container(),
                   ],
-                ),
-                const Divider(
-                  color: AppColors.lightGrey,
-                  height: 1,
-                  thickness: 1,
                 ),
               ],
             ),
