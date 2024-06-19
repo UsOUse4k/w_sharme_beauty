eef70df8d3fe5741c33b2cc7487df31d9ecf8b4e
UsOUse4k <usman.omuraliev.use4k@gmail.com>
2024-06-19 09:20:20 +0600

    added yandex_map for android

--- android/app/src/main/kotlin/com/example/w_sharme_beauty/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/w_sharme_beauty/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.example.w_sharme_beauty
 
-import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity 
+import io.flutter.embedding.engine.FlutterEngine 
+import io.flutter.plugins.GeneratedPluginRegistrant
+import com.yandex.mapkit.MapKitFactory
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        MapKitFactory.setApiKey("f906bf57-b0ed-48a8-a695-c54ea91e962b")
+        super.configureFlutterEngine(flutterEngine)
+    }
+}
