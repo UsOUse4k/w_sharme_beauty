61083f672c1f10f9335f2ad93dec6a054e706adb
Oroz Jumabekov <oroz7307@gmail.com>
2024-06-23 18:38:08 +0600

    fixed

--- lib/features/chat/presentation/widgets/card_message_widget.dart
+++ lib/features/chat/presentation/widgets/card_message_widget.dart
@@ -92,7 +92,6 @@ class CardMessageWidget extends StatelessWidget {
                         if (data != null && data!.posdId != null) {
                           context.push(
                               '/home/chat/chatMessages/${data!.receiverId}/post/${data!.posdId}',);
-
                         }
                       },
                       child: Column(
